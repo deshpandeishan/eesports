201297ce974cd9cf21d1043216ebbafb3f517f21
Ishan Deshpande <deshpandeishan75@gmail.com>
2025-09-22 18:56:32 +0530

    add api.ts

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+export async function registerUser(name: string, email: string, password: string) {
+    const res = await fetch('http://localhost:5000/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, email, password })
+    });
+    return res.json();
+  }
+  
+  
+  
